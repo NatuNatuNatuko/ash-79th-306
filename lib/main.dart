be119d6c7eaf28fc52be300fde81e32fe3f4e56b
haranatsuki <haranatsukiki@gmail.com>
2026-05-22 21:28:47 +0900

    あsdfg

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
